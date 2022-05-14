33aeb90c7ada5ff14ab43427cd84ba3bc0552e99
airusong <airusong.hust@gmail.com>
2022-05-14 21:45:37 +0000

    backend

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.182.0.2:9200"
+	ES_USERNAME = "airusong"
+	ES_PASSWORD = "24131lailai"
+	GCS_BUCKET  = "slai-bucket"
+)
